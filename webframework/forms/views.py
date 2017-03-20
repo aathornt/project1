da82b7b726a26496245cb36146061ed80e376a4f
chase-rydeen <dcrydeen@go.olemiss.edu>
2017-03-20 14:20:37 -0500

    Clear html files to bring in Zack's changes

--- webframework/forms/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import Context, loader
-
-def index(request):
-    template = loader.get_template('forms/index.html')
-    return HttpResponse(template.render())
